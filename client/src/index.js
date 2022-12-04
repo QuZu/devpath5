7e3733e757f8db7aa7ffa80ec215da26d70be245
Enis Mert Kuzu <emertkuzu35@gmail.com>
2022-12-04 13:43:47 +0300

    a

--- client/src/index.js
+++ client/src/index.js
@@ -8,7 +8,7 @@ import reportWebVitals from './reportWebVitals';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename='https://devpath5-yterbe.onrender.com/'>
     <App />
     </BrowserRouter>
   </React.StrictMode>
